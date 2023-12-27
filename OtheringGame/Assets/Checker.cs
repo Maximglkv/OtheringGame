154f25fe17f94b959d57a04cdfde5f02131246d6
Maximglkv <91562210+Maximglkv@users.noreply.github.com>
2023-12-27 17:44:46 +0000

    New pill scene remake
    Needs final script completed, transform parent script is needed for completion to check for added pills.
    Also needs final pill bottle assets.

---
+++ OtheringGame/Assets/Checker.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Checker : MonoBehaviour
+{
+
+    //(transform.parent != null && transform.parent.parent != null)
+
+    /*
+
+    public bool checking;
+
+
+    public void update()
+    {
+        
+    }
+
+
+    public void checker()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(image == image2)
+            {
+                incorrect.SetActive(false);
+                correct.SetActive(true);
+                
+            }
+            else
+            {
+                incorrect.SetActive(true);
+            }
+        }
+    }
+    */
+
+}
